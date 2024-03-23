7123bce7b780b8c66017b7a9f736de5559db35d9
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-03-23 07:45:15 +0100

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/sign-up" Component={Signup} />
+        <Route path="/sign-in" Component={Signin} />
+        <Route path="/Dashboard" Component={Dashboard} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
