b710a808f80f2b40e9798e18dac9d19ae64a5db5
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-03-26 12:22:03 +0100

    connected to database in mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((err) => console.log(err.message));
 
 const app = express();
 
