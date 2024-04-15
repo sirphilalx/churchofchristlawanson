1612d4cb64068cfa410d6dc2e970c19d4a0a14f4
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-04-15 14:10:09 +0100

    created a working login/signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,6 +28,36 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || password === "" || email === "") {
+    next(errorHandler(400, "All Fields are required "));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "wrong credentials"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "wrong credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._d }, process.env.JWT_SECRET_KEY);
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+    console.log(rest);
   } catch (error) {
     next(error);
   }
