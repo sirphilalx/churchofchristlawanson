23a3de7a91932cbe72e31cb13fb2f3ca60710ae8
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-04-21 01:19:48 +0100

    dashnoardProfile update

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -17,7 +17,7 @@ const DashProfile = () => {
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
   const [imageFileUploadError, setImageFileUploadError] = useState(null);
-  console.log(imageFileUploadProgress, imageFileUploadError);
+
   const filePickerRef = useRef();
   const handleImageChange = (e) => {
     const file = e.target.files[0];
